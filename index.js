3d5fd5b484d434b281afd5c5156e72440a972fa3
Jason Garber <jason@sixtwothree.org>
2024-02-25 22:03:47 -0500

    Use pkg as variable name

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const package_ = require("./package.json");
+const pkg = require("./package.json");
 
 module.exports = function(eleventyConfig, options = {}) {
   try {
-    eleventyConfig.versionCheck(package_["11ty"].compatibility);
+    eleventyConfig.versionCheck(pkg["11ty"].compatibility);
   } catch (error) {
-    console.log(`WARN: Eleventy Plugin (${package_.name}) Compatibility: ${error.message}`);
+    console.log(`WARN: Eleventy Plugin (${pkg.name}) Compatibility: ${error.message}`);
   }
 }
